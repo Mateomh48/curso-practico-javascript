1652f896ec9fc81d9ffc4802de63ca78d6ddd698
Mateo Montoya <mateomh48@gmail.com>
2021-07-20 20:01:35 -0500

    Primeros cambios en el proyecto de la calculadora

---
+++ figuras.js
@@ -0,0 +1,9 @@
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es: " + areaCuadrado + "cm^2");
+
