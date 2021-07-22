90db9590bdf8add782c6c4d13fdaf4a8a0253850
Mateo Montoya <mateomh48@gmail.com>
2021-07-22 07:18:08 -0500

    Arreglos realizados en el archivo mediana.js

--- mediana.js
+++ mediana.js
@@ -30,7 +30,7 @@ function esPar(numerito){
 let mediana;
 
 if (esPar(lista1.length)) {
-    const elemento1 = lista1[mitadLista1 - 1];
+    const elemento1 = lista1[mitadLista1 - 1 ];
     const elemento2 = lista1[mitadLista1];
 
     const promedioElemento1y2 = calcularMediaAritmetica([
