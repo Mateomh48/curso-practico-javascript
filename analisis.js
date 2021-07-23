68d2c660ae81457d09cfb8c3e62ab27f9aa0f3cb
Mateo Montoya <mateomh48@gmail.com>
2021-07-22 23:55:08 -0500

    Ultimos anexos

--- analisis.js
+++ analisis.js
@@ -1,3 +1,8 @@
+// Helpers
+function esPar(numerito){
+    return (numerito % 2 === 0);
+}
+
 const salariosCol = colombia.map(
     function(personita){
         return personita.salary;
@@ -10,19 +15,7 @@ const salariosColSorted = salariosCol.sort(
     }
 );
 
-function esPar(numerito){
-    return (numerito % 2 === 0);
-}
-
-function calcularMediaAritmetica(lista){
-    const sumaLista = lista.reduce(
-        function (valorAcumulado = 0, nuevoElemento){
-            return valorAcumulado + nuevoElemento;
-        }
-    );
-    const promedioLista = sumaLista / lista.length;
-    return promedioLista;
-}
+// Calculadora de medianas
 
 function medianaSalarios(lista){
     const mitad = parseInt(lista.length / 2);
@@ -39,6 +32,38 @@ function medianaSalarios(lista){
     }
 }
 
-console.log(
-    medianaSalarios(salariosColSorted));
+// Mediana general
+
+function calcularMediaAritmetica(lista){
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento){
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
+}
+
+const medianaGeneralCol = medianaSalarios(salariosColSorted);
+
+// Mediana del top 10%
+
+
+const spliceStart = (salariosColSorted.length * 90) / 100;
+const spliceCount = salariosColSorted.length - spliceStart;
+
+
+const salariosColTop10 = salariosColSorted.splice(
+    spliceStart,
+    spliceCount,
+);
+
+const medianaTop10Col = medianaSalarios(salariosColTop10);
+
+
+console.log({
+    medianaGeneralCol,
+    medianaTop10Col,
+});
+
 
